7ba75619baf27261bd1e2758f519526b652a4475
IamGreyWolf <zanin@theblackout.de>
2025-04-11 10:56:38 +0200

    Current Update
    Update addons.

---
+++ FontChanger/CustomFontOptions.lua
@@ -0,0 +1,28 @@
+-- DON'T EDIT THIS FILE DIRECTLY -- 
+-- technically you can, but you should really place your 
+-- font files in the fonts/ttfs directory and re-run fonts/slug.bat 
+local FC = FontChanger or {} 
+FC.CUSTOM_FONT_CHOICES = { 
+	"dum1", 
+	"FantaisieArtistique", 
+	"FCBook", 
+	"FCChat", 
+	"FCUI", 
+	"FCUI_Bold", 
+	"LinLibertine_R", 
+	"LinLibertine_RB", 
+	"MagicCardsNormal", 
+	"MorrisRoman-Black", 
+} 
+FC.CUSTOM_FONT_VALUES = { 
+	"FontChanger/fonts/slugs/dum1.slug", 
+	"FontChanger/fonts/slugs/FantaisieArtistique.slug", 
+	"FontChanger/fonts/slugs/FCBook.slug", 
+	"FontChanger/fonts/slugs/FCChat.slug", 
+	"FontChanger/fonts/slugs/FCUI.slug", 
+	"FontChanger/fonts/slugs/FCUI_Bold.slug", 
+	"FontChanger/fonts/slugs/LinLibertine_R.slug", 
+	"FontChanger/fonts/slugs/LinLibertine_RB.slug", 
+	"FontChanger/fonts/slugs/MagicCardsNormal.slug", 
+	"FontChanger/fonts/slugs/MorrisRoman-Black.slug", 
+} 
